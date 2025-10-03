89433a68c9149e124910d05b8f8f3a6e763b0489
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-10-03 18:14:53 +0000

    Sab kus delete kro or new se banao me bolu asha update krte jao

--- next.config.ts
+++ next.config.ts
@@ -10,36 +10,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/recommend-similar-furniture.ts';
-import '@/ai/flows/generate-furniture-configurations.ts';
