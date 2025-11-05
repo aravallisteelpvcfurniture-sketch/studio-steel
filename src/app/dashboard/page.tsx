65ea3c1046beebadc47a6288194d8a113dc7e5bb
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-05 18:18:07 +0000

    Ye desbord wali design hi ye thodi niche kro upr me welcome niche dab ra

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -48,7 +48,7 @@ export default function DashboardPage() {
           </div>
         </div>
       </header>
-      <div className="bg-background rounded-t-[3rem] p-8 -mt-8 relative text-foreground">
+      <div className="bg-background rounded-t-[3rem] p-8 -mt-12 relative text-foreground">
         <div className="relative mb-6">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
           <Input
