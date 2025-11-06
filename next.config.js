adc37af64f04b9623945d19dc64694e2c785c445
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-06 09:17:07 +0000

    Abhi error aara hi google se signup nhi hora hai to update kro

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'studio-8143900809-49441.appspot.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
