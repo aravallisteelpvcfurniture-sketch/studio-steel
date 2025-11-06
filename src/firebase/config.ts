c4f99a57a94c88c0935f1f50c5a31640eb063951
Harishpatel Harishpatel <harishpatelh450@gmail.com>
2025-11-06 10:47:20 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,5 +4,6 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyBaPDg0JEjj4ZM24RhhvHOWq61NJW6vd6E",
   "authDomain": "studio-8143900809-49441.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "638349737153"
+  "messagingSenderId": "638349737153",
+  "storageBucket": "studio-8143900809-49441.appspot.com"
 };
